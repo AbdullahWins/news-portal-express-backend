18be765be2387b00bfab9084d1120f7f5320abfe
AbdullahWins <noyononline@gmail.com>
2022-10-20 23:25:39 +0600

    added more functionality to the server

--- index.js
+++ index.js
@@ -12,12 +12,28 @@ app.get("/", (req, res) => {
   res.send("news api running!");
 });
 
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
 app.get("/catagories", (req, res) => {
   res.send(catagories);
 });
 
-app.get("/news", (req, res) => {
-  res.send(news);
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
+
+app.get("/catagories/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "08") {
+    res.send(news);
+  } else {
+    const selectedCatagory = news.filter((n) => n.category_id === id);
+    res.send(selectedCatagory);
+  }
 });
 
 app.listen(port, () => {
